1de308af10d0555bf1971e5df7ba6c167529f8f4
agdev04 <dev@agnieve.com>
2025-04-13 11:13:04 +0800

    fixed database error

--- celery_worker.py
+++ celery_worker.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from models import User, ChatSession, ChatMessage, Base, SessionLocal  # reuse models
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_recycle=280, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 celery_app = Celery(
--- models.py
+++ models.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 import datetime
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_recycle=280, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
